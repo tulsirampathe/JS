1d20f17b6d44c316a711d4a4a12872026c98eafa
TULSIRAM PATHE <145448779+tulsirampathe@users.noreply.github.com>
2024-08-18 16:28:34 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+    + 3
+) // code readability should be high
+
+console.log("Tulsiram Pathe")
+
+let name = "Tusliram"
+let age = 19
+let isLoggedIn = false
+let state
+
+// Premitive DataType
+// number => 2 to power of 53
+// bigint 
+// string => "", ''
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// Non Promitive DataType
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
