ed61e0fd61c9a5e0e8c5c8bfd2b00a48ae961ce3
TULSIRAM PATHE <145448779+tulsirampathe@users.noreply.github.com>
2024-08-19 06:43:04 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,6 +9,7 @@ let valueInNumber = Number(score)
 
 // "33" => 33
 // "33abc" => NaN (Not a Number)
+// "" => 0
 // true => 1; false => 0
 
 let isLoggedIn = "pathe"
@@ -23,5 +24,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ********************* Operations ******************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2)
+// console.log(2**3) => ** for power of any number
+// console.log(2/3)
+// console.log(2%3)
+
+
+let str1 = "hello"
+let str2 = " tulsiram"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2")
+
+// console.log( (3+4) * 5 % 3)
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+// console.log(gameCounter++) // 100
+// console.log(++gameCounter) // 101
+
