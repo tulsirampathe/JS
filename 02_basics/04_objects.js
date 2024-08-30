ca7f33e38e1d9590465bee3cd6b1f2a69a9a167d
TULSIRAM PATHE <145448779+tulsirampathe@users.noreply.github.com>
2024-08-30 16:36:14 +0000

    json formate

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -21,14 +21,14 @@ const regularUser = {
 
 // console.log(regularUser.fulname.userfullname.firstname);
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {3: "c", 4: "d"}
-const obj4 = {5: "c", 6: "d"}
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "c", 4: "d" }
+const obj4 = { 5: "c", 6: "d" }
 
 // const obj3 = {obj1, obj2}
 // const obj3 = Object.assign({} ,obj1, obj2, obj4)
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = { ...obj1, ...obj2 }
 // console.log(obj3)
 
 const users = [
@@ -46,11 +46,39 @@ const users = [
     },
 ]
 
-console.log(users[1].id)
-console.log(tinderUser)
+// console.log(users[1].id)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser)) // get all keys of the object
-console.log(Object.values(tinderUser)) // get all values of the object
-console.log(Object.entries(tinderUser)) 
+// console.log(Object.keys(tinderUser)) // get all keys of the object
+// console.log(Object.values(tinderUser)) // get all values of the object
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn')) // check the property is exits or not 
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')) // check the property is exits or not 
+
+// Destructuring
+const course = {
+    coursename: "js in Hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+const {courseInstructor: instructor} = course;
+
+// console.log(courseInstructor)
+console.log(instructor)
+
+
+// API : JSON object
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {},
+//     {},
+// ]
+
