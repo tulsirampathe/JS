19288aeff4eacb33ae69b6ebe9dc64edb8bff368
TULSIRAM PATHE <145448779+tulsirampathe@users.noreply.github.com>
2024-09-06 17:21:26 +0000

    For loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,44 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+
+    if (element == 5) {
+        // console.log("5 is best number")
+    }
+    // console.log("ind : ", element)
+}
+// console.log(element)
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`)
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner loop value ${j} and inner loop ${i}`)
+        // console.log(i + '*' + j + ' = ' + i * j)
+    }
+}
+
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+for (let i = 0; i < myArray.length; i++) {
+    // console.log(myArray[i])
+}
+
+// break and continue
+
+// for (let i = 1; i <= 20; i++) {
+    // if (i == 5) {
+        // console.log("Detected 5")
+        // break;
+    // }
+    // console.log(`Value of i : ${i}`)
+// }
+// 
+
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log("Detected 5")
+        continue; // one time ignore
+    }
+    console.log(`Value of i : ${i}`)
+}
