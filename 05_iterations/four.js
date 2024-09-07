eb4ae8c8fddf345651ea9f28c0c1aa8dfe1ba861
TULSIRAM PATHE <145448779+tulsirampathe@users.noreply.github.com>
2024-09-07 11:53:02 +0000

    loops discussion

---
+++ 05_iterations/four.js
@@ -0,0 +1,28 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    switf: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for(const key in programming)
+{
+    // console.log(programming[key])
+}
+
+// map is not iterable
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+
+// for(const key in map)
+// {
+//     console.log(key)
+// }
